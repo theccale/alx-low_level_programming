98db5f8300044426352f40a05cc551014dcae844
Oluwatobi Alani <adebayodeboraholuwatobi@gmail.com>
2023-08-11 05:46:49 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,6 +4,5 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{printf("with proper grammar, but the outcome is a piece of art, ");
-    return (0);}
+int main(void){printf("with proper grammar, but the outcome is a piece of art,");
+    return (0); }
