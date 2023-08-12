44872173287fcea4b51e281202fa73f375da4480
Oluwatobi Alani <adebayodeboraholuwatobi@gmail.com>
2023-08-11 21:29:51 -0700

    0-positive_or_negagive.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,20 +12,20 @@ int main(void)
 	int x;
 
 	srand(time(0));
-	x= rand() RAND MAX / 2;
+	x= rand() - RAND MAX / 2;
 
 	printf("%d\n",x);
 	if (x > 0)
 	{
-		printf("The number is positive \n")
+		printf("The number is positive \n");
 	}
 	else if(x < 0)
 	{
-		printf("The number is negative \n")
+		printf("The number is negative \n");
 	}
 	else
 	{
-		printf("The number is zero \n")
+		printf("The number is zero \n");
 	}
 	return 0;
 }
