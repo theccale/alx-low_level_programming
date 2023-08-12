ace0a83288d1b49774dad40d8be9fde412b08499
Oluwatobi Alani <adebayodeboraholuwatobi@gmail.com>
2023-08-11 19:11:45 -0700

    git commit -m "0-positive_or_negative.c"

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <time.h>
 /**
- *main
+ *main -Entry point
  *
  *Return: Always 0(Success)
  */
@@ -14,7 +12,6 @@ int main()
 	int x:
 
 	srand(time(0)):
-
 	x= rand() RAND MAX / 2:
 
 	printf("%d\n",x);
@@ -31,6 +28,5 @@ int main()
 	{
 		printf("The number is zero \n")
 	}
-
 	return 0;
 }
