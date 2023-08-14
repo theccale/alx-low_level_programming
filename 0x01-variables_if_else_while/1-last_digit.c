0d3b3de8b865c143dbf7ac34e2ffcde5178ad006
Oluwatobi Alani <adebayodeboraholuwatobi@gmail.com>
2023-08-13 18:50:59 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,9 +8,10 @@
  */
 int main()
 {	int n;
+	int x = (n % 10);
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int x = (n % 10);
+
 	if (x > 5)
 	{	printf("The Last digit of %d is %d and is greater than 5\n", n, x);
 	}
