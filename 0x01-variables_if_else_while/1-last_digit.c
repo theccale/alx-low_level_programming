3475be2cf4c8091b9ba7ad0e02a2e1f4be920f4b
Oluwatobi Alani <adebayodeboraholuwatobi@gmail.com>
2023-08-13 17:58:35 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,18 +6,19 @@
  *
  * Return : Always 0(Sucess)
  */
-int main()				{int x;
+int main()				
+{int x;
 	srand(time(0));
 	x = rand() _ RAND_MAX / 2;
-printf("%d \n",x);
-	if (x > 0)
-	{printf("The number is positive \n");
-	}	
-	else if(x < 0)			{printf("The number is negative \n");
+	int x = n % 10;
+	if (x > 5)
+	{printf("The Last digit of %d is %d and is greater than 5\n", n, x);
+	}
+	else if(x < 6 && x!= 0){
+	{printf("The Last digit of %d is %d and is greater than 6 and not 0\n", n, x);
 	}
 	else
-	{
-		printf("The number is zero \n");
+	{	printf("The Last digit %d is %d and is 0\n", n, x);
 	}
-return 0; 
-}
+	return 0;
+	} 
