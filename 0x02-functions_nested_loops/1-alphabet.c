c28ba0e1df284a1783a2842f9de9d0e16df0a8a7
Oluwatobi Alani <adebayodeboraholuwatobi@gmail.com>
2023-08-15 16:46:17 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: print_ alphabet in lower case
+ *
+ * Return:0
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <='z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
