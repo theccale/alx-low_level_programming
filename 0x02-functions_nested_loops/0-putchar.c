ccdd5e8816c9ebb6d28b0b666b544a264427ee27
Oluwatobi Alani <adebayodeboraholuwatobi@gmail.com>
2023-08-15 14:15:48 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* Main - prints a putchar commands 
+ * 	that returns 0 at success
+ * Return: Always (0) 
+ */
+
+int _putchar(char c)
+{ 
+	return _putchar; 
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+making use of nested loops functions
