41fa3659da77ac2d46c87df6c9513eeac2987e02
Oluwatobi Alani <adebayodeboraholuwatobi@gmail.com>
2023-08-15 19:05:06 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - checks for alphabetic character
+ * @c:the chatacter to be checked
+ *
+ * Return:1 if c is a letter 
+ * otherwise 0
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
