c806d6e30a7c1feb362cf7e4df497bfbc6736169
Oluwatobi Alani <adebayodeboraholuwatobi@gmail.com>
2023-08-15 18:50:05 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,15 +2,16 @@
 /**
  * main - Entry point
  *
- * Description: _islower checks for
- * 	lowercase character
+ * Description: _islower checks for lowercase character
  *
+ * @c:is the char to be checked
  * Return: 1 if lowercase
  *	otherwise  0
  */
 
-int _islower (int c)
-{if (c >= 'a' && c <= 'z')
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
 	return (1);
 	else
 	return (0);
