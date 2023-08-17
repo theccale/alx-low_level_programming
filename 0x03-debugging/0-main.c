5b239af4b41ffe7e810bf721675deeee804117b1
Oluwatobi Alani <adebayodeboraholuwatobi@gmail.com>
2023-08-17 09:23:00 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * main - tests function that prints if integer is positive or negative
+ * main - tests function that prints if
+ * integer is positive or negative
  * Return: 0
  */
 
@@ -10,6 +11,5 @@ int main(void)
 	int i;
 	i = 0;
 	positive_or_negative(i);
-	
-	return (0);
+		return (0);
 }
