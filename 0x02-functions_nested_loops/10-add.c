2415d868ffca1007e76de83513c2f3355056e4d8
Oluwatobi Alani <adebayodeboraholuwatobi@gmail.com>
2023-08-16 19:32:59 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * add - Adds two integers
- * @n1 - The first integer to be added
- * @n2 - The second integer to be added
+ * @n1: The first integer to be added
+ * @n2: The second integer to be added
  *
  * Return: interger value
  */
