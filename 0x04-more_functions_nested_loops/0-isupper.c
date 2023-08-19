5455e08b79a22719da3f51737b7713cd48c7bc64
Oluwatobi Alani <adebayodeboraholuwatobi@gmail.com>
2023-08-18 18:29:35 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,12 @@
 
 /**
  * -isupper - checks for uppercase character.
- *@c: The number to be checked
+ * @c: The number to be checked
  * Return: 1 if c is uppercase or 0 if otherwise
  */
 int _isupper(int c)
 {
-   if (c >= 65 && c <= 90)
+   	if (c >= 65 && c <= 90)
 	{
 	return (1);
 	}
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  _isdigit - checks for a digit (0 through 9).
+ *  @x: The character to be tested
+ *  Returns: 1 if c is a digit otherwise 0
+ */
+
+int _isdigit(int c)
+{
+	if (x >= 48 && x <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
