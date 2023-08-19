d973ef11b235eb79d6ce649354cef920a25a31bb
Oluwatobi Alani <adebayodeboraholuwatobi@gmail.com>
2023-08-18 18:02:44 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * -isupper - checks for uppercase character.
+ *@c: The number to be checked
+ * Return 1 if c is uppercase or 0 if otherwise
+ */
+int _isupper(int c);
+{
+   if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+    return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+more files on nested loops
