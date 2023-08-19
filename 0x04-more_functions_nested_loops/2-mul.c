26c4f350dde235979f9137fad96c3afe636e4872
Oluwatobi Alani <adebayodeboraholuwatobi@gmail.com>
2023-08-18 19:08:50 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * -mul - multiplies two integers
+ * @a: The first number to be multiplied
+ * @b: The second number to be multiplied with the first
+ *
+ * Return: The result of the multiplication between the numbers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
