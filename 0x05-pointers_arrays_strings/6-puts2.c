dc418a6950d29ca2a7f9fc4f842b356c29f45f0d
Oluwatobi Alani <adebayodeboraholuwatobi@gmail.com>
2023-08-21 20:46:34 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,9 +2,9 @@
 
 /**
   * puts2 - Prints every other character of a string,
-  *  starting with the first character,
-  *  followed by a new line.
-  * @str: The string to be considered
+  * starting with the first character,
+  * followed by a new line.
+  *@str: The string to be considered
   *
   * Return: void
   */
