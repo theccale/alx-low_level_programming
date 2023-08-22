3e233657a98f10bfdb01c0d299d6c1d02a6caf18
Oluwatobi Alani <adebayodeboraholuwatobi@gmail.com>
2023-08-21 21:07:08 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,1 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
 
+/**
+ * -keygen - generates random valid passwords
+ *  for the program 101-crackme.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{       int password [64];
+        int i;
+        int sum = 0;
+        int n;
+        srand (time(null));
+        for (i = 0; i < 64; i++)
+        {
+                password [i] = rand() % 78 ;
+                sum += password [i] + '0';
+                putchar (password [i] + '0');
+
+                if ((2772 - sum) - '0' < 78)
+                {
+                        n = 2772 - sum - '0';
+                        sum += n;
+                        putchar (n + '0');
+                        break ;
+                }
+        }       return (0);
+}
