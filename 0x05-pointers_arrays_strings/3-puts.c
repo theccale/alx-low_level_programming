926923730ac4556e59492ecd29423027ada7bf1f
Oluwatobi Alani <adebayodeboraholuwatobi@gmail.com>
2023-08-21 17:14:34 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * _puts - Prints a string, followed by a new line, to stdout.
+  * @str: The string to print
+  *
+  * Return: void
+  */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * print_rev - Prints a string in reverse,
+  * followed by a new line.
+  * @s: The string to print
+  *
+  * Return: void
+  */
+void print_rev(char *s)
+{
+	int k = 0;
+
+	while (s[k] != '\0')
+	{
+		c++;
+	}
+
+	for (k -= 1; k >= 0; k--)
+	{
+		_putchar(s[k]);
+	}
+
