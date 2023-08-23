a137e0d69a6fc6e94f6f26b2b13a8a30570da218
Oluwatobi Alani <adebayodeboraholuwatobi@gmail.com>
2023-08-22 17:49:19 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,8 +12,10 @@ void rev_string(char *s)
 	int counter = 0;
 	int i;
 	while (s[counter] != '\0')
+
 	       counter++;
-	for (i = 0; i < counter; i++)
+		for (i = 0; i < counter; i++)
+
 	{
 		counter--;
 		rev = s[i];
