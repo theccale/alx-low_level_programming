9ffdd42eb91e517c037c7dec819ce03164afb35b
Oluwatobi Alani <adebayodeboraholuwatobi@gmail.com>
2023-08-24 21:03:32 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -4,11 +4,11 @@
 /**
  * *string_toupper - changes all lowercase letters
  * of a string to uppercase
- * *p: The string too be changed
+ * p: The string to be changed
  *
  * Return: Always 0.
  */
-char *string_toupper(char *p)
+char *string_toupper(char *)
 {
 	int x = 0;
 
