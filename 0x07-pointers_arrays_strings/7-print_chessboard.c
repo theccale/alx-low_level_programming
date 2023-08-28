6fc57525d070b6255ad40559b748390421d54e22
Oluwatobi Alani <adebayodeboraholuwatobi@gmail.com>
2023-08-29 00:40:47 +0100

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ *
+ * @a: the row of the array
+ *
+ * Return: nothing
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int c, b;
+
+	for (c = 0; c < 8; c++)
+	{
+		for (b = 0; b < 8; b++)
+		{
+			if (b == 7)
+			{
+				_putchar(a[c][b]);
+				_putchar('\n');
+			}
+			else
+				_putchar(a[c][b]);
+		}
+	}
