444b5aef24b8a222df100ce968ad9c61742b5cf0
Oluwatobi Alani <adebayodeboraholuwatobi@gmail.com>
2023-08-31 04:09:31 +0100

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,9 +8,11 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (s[0] != '\0')
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
+	else
 		_putchar('\n');
-			_putchar(*s);
-	_puts_recursion(++s);
-
 }
