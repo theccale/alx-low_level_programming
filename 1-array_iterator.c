5ebbbb0bc7915cc0505717a75360b5a13e7b2c88
Oluwatobi Alani <adebayodeboraholuwatobi@gmail.com>
2023-09-16 02:50:55 +0100

    tasks on function pointers

---
+++ 1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+* array_iterator - prints each array element on a newline
+* @array: array
+* @size: size of the array
+* @action: pointer to the function that prints the array
+* Return: void
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
+
---
+++ 2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+/**
+* int_index - searches for an integer and
+* return index place if comparison = true, else -1
+* @array: array
+* @size: size of elements in array
+* @cmp: pointer to the function that compares  values
+* Return: -1 if no element matches,
+* if size <=0, -1
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
+
