98acfc799fec3d06c28c83ba96e53cbd230046f5
Oluwatobi Alani <adebayodeboraholuwatobi@gmail.com>
2023-09-24 16:18:10 -0700

    morre varaiadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,28 +1,11 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 /**
 * print_all - prints anything
 * @format: list of types of arguments passed to the function
-=======
-/**
-* print_all - prints anything
-* @format: list of types of arguments passed to the function
-* ...: other arguments
-* Return: Always 0
->>>>>>> 8cc1cabe55448f952769eb41bf61eb26148f02e2
 */
-=======
-
-/**
- * print_all - prints anything
- * @format: list of types of arguments passed to the function
- * @...: other variables
- */
->>>>>>> b55796704233a86fec32aba79a0bba1f4514e203
 void print_all(const char * const format, ...)
 {
 	int i = 0;
@@ -64,91 +47,5 @@ void print_all(const char * const format, ...)
 
 	printf("\n");
 	va_end(list);
-<<<<<<< HEAD
 }
 
-<<<<<<< HEAD
-=======
-int a = 0;
-
-char *str, *virt = "";
-
-
-
-va_list others;
-
-
-
-va_start(others, format);
-
-
-if (format)
-
-{
-
-while (format[a])
-
-{
-
-switch (format[a])
-
-{
-
-case 'c':
-
-printf("%s%c", virt, va_arg(others, int));
-
-break;
-
-case 'i':
-
-printf("%s%d", virt, va_arg(others, int));
-
-break;
-
-case 'f':
-
-printf("%s%f", virt, va_arg(others, double));
-
-break;
-
-case 's':
-
-str = va_arg(others, char *);
-
-if (!str)
-
-str = "(nil)";
-
-printf("%s%s", virt, str);
-
-break;
-
-default:
-
-a++;
-
-continue;
-
-}
-
-virt = ", ";
-
-a++;
-
-}
-
-}
-
-
-
-printf("\n");
-
-va_end(virt);
-
-}
->>>>>>> 8cc1cabe55448f952769eb41bf61eb26148f02e2
-=======
-}
-
->>>>>>> b55796704233a86fec32aba79a0bba1f4514e203
