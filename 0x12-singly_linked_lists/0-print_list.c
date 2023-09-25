76dcbedac906603a715785468c5df7b474c080bb
Oluwatobi Alani <adebayodeboraholuwatobi@gmail.com>
2023-09-24 17:39:22 -0700

    updated task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,7 +4,7 @@
 /**
 * print_list - prints all the elements of a linked list
 * @h: pointer to the list_t list to print
-* Return: the number of nodes
+* Return: the number of elements
 */
 size_t print_list(const list_t *h)
 {
