55ba37bd727b1df052cfe71e11352732e1e6ffe5
Oluwatobi Alani <adebayodeboraholuwatob@gmail.com>
2023-09-27 15:48:51 +0100

    mor efiles

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,7 +9,9 @@ size_t print_listint(const listint_t *h)
 
 	size_ num = 0;
 	while (h)
+	
 	{
+		
 		printf("%d\n", h->n);
 		num++;
 		h = h->next;
---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * sum_listint - returns the sum of all the data (n) of a listint_t linked list
+ * @head: pointer to the first element in the list
+ *
+ * Return: sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *temp = head;
+}
