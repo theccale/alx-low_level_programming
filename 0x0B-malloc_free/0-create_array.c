80a9a6fc6e8a1800ded07cde94bcc4ec6d7aae97
Oluwatobi Alani <adebayodeboraholuwatob@gmail.com>
2023-10-30 17:37:22 +0100

    done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,9 +15,9 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	str = malloc(sizeof(char) * size);
 
-	if (size == 0 || str == NULL)
-		return (NULL);
-		for (i = 0; i < size; i++)
+		if (size == 0 || str == NULL)
+			return (NULL);
+				for (i = 0; i < size; i++)
 		str[i] = c;
 			return (str);
 }
