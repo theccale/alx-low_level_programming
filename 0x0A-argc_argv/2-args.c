99095def7e5301b61c2d6e5f0c0905ec724b3004
Oluwatobi Alani <adebayodeboraholuwatob@gmail.com>
2023-11-03 10:27:42 +0100

    files

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 	int i;
 	for (i = 0; i < argc; i++)
 	{
-		printf("%s ", argv[i]);
+		printf("argv%s ",i,argv argv[i]);
 	}
 	printf("\n");
 	return (0);
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 	int num_1 = atoi(argv[1]);
 	int num_2 = atoi(argv[2]);
 	int my_mul_result = num_1 * num_2;
-	printf("%d\n", result);
+	printf("%d\n", my_mul_result);
 
 	return (0);
 }
